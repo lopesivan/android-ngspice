8321729dfc285c3aa528eb779a9e082a8ead5a30
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2017-05-09 15:33:49 -0300

    Adiciona o arquivo ngproc2mod.sh

---
+++ ngproc2mod.sh
@@ -0,0 +1,10 @@
+#!/system/bin/sh
+
+LD=/data/data/com.droidplot/mylib/ld-linux.so.3
+aLIB=/data/data/com.droidplot/mylib
+bLIB=/data/data/com.octave.arm/freeRoot/usr/local/lib
+BIN=/data/data/com.octave.arm/freeRoot/usr/local/bin/ngproc2mod
+
+$LD --library-path ${aLIB}:${bLIB} $BIN $@
+
+exit 0
